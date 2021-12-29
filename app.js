0f1525485e3320e2ec71b9b6a0e28f1eaa883550
Imane Elouazzani <denjala.smiley@gmail.com>
2021-12-29 21:03:07 +0100

    projet terminé

---
+++ app.js
@@ -0,0 +1,59 @@
+const btns = document.querySelectorAll('.btn-modale')
+const modaleEquipement = document.querySelector('.bloc-modale')
+const imgIndex = document.querySelector('.bloc-modale img')
+
+// if(window.matchMedia("(min-width: 850px)").matches){
+//     btns.forEach(btn => { 
+//     btn.addEventListener('click', (e) => {
+//         imgIndex.src = `ressources/img${e.target.getAttribute('data-index')}-equip.jpg`
+//         modaleEquipement.classList.add('active-modale')
+//         })
+//     })
+
+//     modaleEquipement.addEventListener('click', e => {
+//     modaleEquipement.classList.remove('active-modale')
+//     })
+// }   
+
+window.addEventListener("resize", () => {
+    responsiveModals();
+});
+
+function responsiveModals() {
+    if (window.matchMedia("(min-width:850px)").matches) {
+        btns.forEach((btn) => {
+            btn.addEventListener("click", clickBtn);
+        });
+
+        modaleEquipement.addEventListener("click", () => {
+            modaleEquipement.classList.remove("active-modale");
+        });
+    } else {
+        btns.forEach((btn) => {
+            btn.removeEventListener("click", clickBtn);
+        });
+    }
+}
+responsiveModals();
+
+function clickBtn(e) {
+    imgIndex.src = `ressources/img${e.target.getAttribute(
+        "data-index"
+    )}-equip.jpg`;
+    modaleEquipement.classList.add("active-modale");
+}
+
+// Anim navbar
+
+const nav = document.querySelector('nav')
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 30) {
+        nav.classList.add('anim-nav')
+    } else {
+        nav.classList.remove('anim-nav')
+    }
+})
+
+
+
